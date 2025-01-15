0211f33f444361cf68520a438a824444f20c733e
lepton <tonihirvonen00@gmail.com>
2025-01-16 00:48:31 +0200

    term size

--- include/tui.h
+++ include/tui.h
@@ -2,8 +2,9 @@
 #define TUI_H
 
 #ifdef __linux__
-//#include <unistd.h>
+#include <unistd.h>
 #include <ncurses.h>
+#include <sys/ioctl.h>
 #endif
 #ifdef _WIN32
 #include <windows.h>
@@ -11,11 +12,17 @@
 #include <conio.h>
 #endif
 
+typedef struct {
+  int rows;
+  int cols;
+} term_size;
+
 void altBufferEnable();
 void altBufferDisable();
 void cls(FILE* s);
 char keyPress();
 void initScreen();
 char initScreenWin();
+void getTermSize(term_size* term);
 
 #endif
--- src/tui.c
+++ src/tui.c
@@ -53,3 +53,27 @@ char initScreenWin() {
     return 1;
 }
 
+void getTermSize(term_size* term) {
+#ifdef _WIN32
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi)) {
+        term->cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+        term->rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    } else {
+        fprintf(stderr, "Error: Unable to get console screen buffer info.\n");
+        term->rows = 0;
+        term->cols = 0;
+    }
+#else
+    struct winsize w;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) == -1) {
+        perror("ioctl");
+        term->rows = 0;
+        term->cols = 0;
+    } else {
+        term->rows = w.ws_row;
+        term->cols = w.ws_col;
+    }
+#endif
+}
+
