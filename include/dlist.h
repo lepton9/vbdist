484c2b507da5ff78e2d98323bb37691cdda33ccc
lepton <tonihirvonen00@gmail.com>
2025-01-19 16:41:37 +0200

    dynamic list

---
+++ include/dlist.h
@@ -0,0 +1,17 @@
+#ifndef DLIST_H
+#define DLIST_H
+
+#include <stdlib.h>
+
+typedef struct {
+  void** items;
+  size_t n;
+  size_t size;
+  size_t item_size;
+} dlist;
+
+dlist* init_list(size_t item_size);
+void free_list(dlist* list);
+void list_add(dlist* list, void* item);
+
+#endif
---
+++ src/dlist.c
@@ -0,0 +1,25 @@
+#include "../include/dlist.h"
+
+dlist *init_list(size_t item_size) {
+  dlist *list = malloc(sizeof(dlist));
+  list->n = 0;
+  list->size = 1;
+  list->item_size = item_size;
+  list->items = malloc(list->item_size);
+  return list;
+}
+
+void free_list(dlist* list) {
+  if (list->items) {
+    free(list->items);
+  }
+  free(list);
+}
+
+void list_add(dlist *list, void *item) {
+  if (list->n == list->size) {
+    list->size *= 2;
+    list->items = realloc(list->items, list->size * list->item_size);
+  }
+  list->items[list->n++] = item;
+}
