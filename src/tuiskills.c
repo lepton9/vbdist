55f38d2baf05ab0c8dc9e96acab5e354548dd0fc
lepton <tonihirvonen00@gmail.com>
2025-03-03 02:21:33 +0200

    renders skill tui list

--- src/tuiskills.c
+++ src/tuiskills.c
@@ -1,4 +1,5 @@
 #include "../include/tuiskills.h"
+#include <stdlib.h>
 
 tui_skills* init_tui_skills(sqldb* db, dlist* skills, dlist* selectedSkills) {
   tui_skills* tui = malloc(sizeof(tui_skills));
@@ -11,6 +12,7 @@ tui_skills* init_tui_skills(sqldb* db, dlist* skills, dlist* selectedSkills) {
   tui->skills_area = init_list_area(tui->term->cols, tui->term->rows);
   tui->skills = skills;
   tui->selected_skill_ids = selectedSkills;
+  update_list_len(tui->skills_area, tui->skills->n);
   // tui->skills = fetchSkills(db);
 
   return tui;
@@ -66,6 +68,28 @@ void handleSkillsInput(tui_skills *tui, int c) {
   }
 }
 
+int is_selected_skill(skill* skill, dlist* selected_ids) {
+  for (size_t i = 0; i < selected_ids->n; i++) {
+    if (skill->id == *(int*)selected_ids->items[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+void toggle_selected_skill(tui_skills* tui) {
+  skill* selected = tui->skills->items[tui->skills_area->selected];
+  int i = is_selected_skill(selected, tui->selected_skill_ids);
+  if (i >= 0) {
+    int* id = pop_elem(tui->selected_skill_ids, i);
+    free(id);
+  } else {
+    int* id = malloc(sizeof(int));
+    *id = selected->id;
+    list_add(tui->selected_skill_ids, id);
+  }
+}
+
 void update_skills_area(tui_skills* tui) {
   getTermSize(tui->term);
   int rows = tui->term->rows - 2;
@@ -75,7 +99,31 @@ void update_skills_area(tui_skills* tui) {
 }
 
 void renderSkillsTui(tui_skills* tui) {
-  // TODO: print ui
+  append_line(tui->render, 0, "\033[4m %s \033[24m", "Selected skills");
+
+  // TODO: make func to iterate tui lists
+  int line = 2;
+  for (int i = tui->skills_area->first_ind;
+  line <= tui->term->rows - 1 &&
+  i - tui->skills_area->first_ind + 1 <= (int)tui->skills_area->max_shown &&
+  i < (int)tui->skills->n;
+  i++) {
+    skill* cur_skill = tui->skills->items[i];
+
+    if (tui->skills_area->selected == i) {
+      tui->skills_area->selected_term_row = line + 1;
+      append_line(tui->render, line, "\033[7m");
+    }
+    if (is_selected_skill(cur_skill, tui->selected_skill_ids) >= 0) {
+      append_line(tui->render, line, ">");
+    }
+
+    append_line(tui->render, line, " %-20s", cur_skill->name);
+    if (tui->skills_area->selected == i) {
+      append_line(tui->render, line, "\033[27m");
+    }
+    line++;
+  }
 
   render(tui->render);
 }
