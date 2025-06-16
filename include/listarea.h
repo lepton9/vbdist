1920d19d3217a4a6f588cb0f524dff8a7645229d
lepton <tonihirvonen00@gmail.com>
2025-06-16 13:48:27 +0300

    tui area

--- include/listarea.h
+++ include/listarea.h
@@ -4,14 +4,31 @@
 #include <stdlib.h>
 
 typedef struct {
+  size_t height;
+  size_t width;
+  size_t start_row;
+  size_t start_col;
+  int padding_top;
+  int padding_bottom;
+} tui_area;
+
+typedef struct {
+  tui_area* area;
   size_t len;
   size_t max_shown;
-  size_t width;
   int first_ind;
   int selected;
   int selected_term_row;
 } list_area;
 
+
+tui_area* init_tui_area(size_t w, size_t h, size_t row, size_t col);
+void free_tui_area(tui_area* a);
+
+void set_area_pos(tui_area* area, size_t row, size_t col);
+void set_padding(tui_area* area, int padding_top, int padding_bottom);
+int start_print_line(tui_area* area);
+int area_last_line(tui_area* area);
 
 list_area* init_list_area(size_t w, size_t h);
 void free_list_area(list_area* la);
--- src/listarea.c
+++ src/listarea.c
@@ -1,17 +1,55 @@
 #include "../include/listarea.h"
 #include "../include/utils.h"
 
+tui_area* init_tui_area(size_t w, size_t h, size_t row, size_t col) {
+  tui_area* area = malloc(sizeof(tui_area));
+  area->width = w;
+  area->height = h;
+  area->start_row = row;
+  area->start_col = col;
+  area->padding_top = 0;
+  area->padding_bottom = 0;
+  return area;
+}
+
+void free_tui_area(tui_area* a) {
+  free(a);
+}
+
+void set_area_pos(tui_area* area, size_t row, size_t col) {
+  area->start_row = row;
+  area->start_col = col;
+}
+
+void set_padding(tui_area* area, int padding_top, int padding_bottom) {
+  area->padding_top = padding_top;
+  area->padding_bottom = padding_bottom;
+}
+
+void update_area(tui_area* area, size_t w, size_t h) {
+  area->width = w;
+  area->height = h;
+}
+
+int start_print_line(tui_area* area) {
+  return area->start_row + area->padding_top;
+}
+
+int area_last_line(tui_area* area) {
+  return area->height + area->start_row - 1;
+}
 
 list_area* init_list_area(size_t w, size_t h) {
   list_area* area = malloc(sizeof(list_area));
+  area->area = init_tui_area(w, h, 0, 0);
   area->first_ind = 0;
   area->selected = -1;
-  area->width = w;
   area->max_shown = h;
   return area;
 }
 
 void free_list_area(list_area* la) {
+  free_tui_area(la->area);
   free(la);
 }
 
@@ -46,8 +84,8 @@ void fit_screen(list_area* la) {
 }
 
 void update_list_area(list_area* la, size_t w, size_t h) {
-  la->width = w;
-  la->max_shown = h;
+  update_area(la->area, w, h);
+  la->max_shown = max_int(0, h - start_print_line(la->area) - la->area->padding_bottom);
   check_selected(la);
   fit_screen(la);
 }
