8cf3b5c1bfc26c3533d382fe5077bae99a490ca2
lepton <tonihirvonen00@gmail.com>
2025-09-22 14:17:04 +0300

    feat: options in a static array

--- include/args.h
+++ include/args.h
@@ -11,6 +11,29 @@ typedef enum {
 } action;
 
 typedef struct {
+  const char* name_short;
+  const char* name_long;
+  struct {
+    const char* name;
+    char required;
+  } arg;
+  const char* description;
+  char required;
+} option;
+
+#define OPTIONS_N 8
+
+static const option options[OPTIONS_N] = {
+    {"d", "database", {"path", 1}, "Path to sqlite database", 0},
+    {"f", "file", {"path", 1}, "Path to textfile", 0},
+    {"t", "teams", {"int", 1}, "Set number of teams", 0},
+    {"p", "players", {"int", 1}, "Set number of players in a team", 0},
+    {"c", "config", {NULL}, "Print config location", 0},
+    {"l", "log", {"path", 1}, "Set custom log file path", 0},
+    {"v", "viewlog", {"int", 0}, "Print the last rows in log (default: 10)", 0},
+    {"h", "help", {NULL}, "Print this help", 0}};
+
+typedef struct {
   char *filePath;
   char *dbPath;
   char *logPath;
